3c1d07be1280ece505270200ffb0ac3304c14f34
wangfucheng <wangfucheng@machloop.com>
2020-10-12 17:55:38 +0800

    feat: commit

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 ## 参考
 
 - [SELECT Query](https://clickhouse.tech/docs/en/sql-reference/statements/select/)
+
+
+source=table search a=1 and ( b= 2 or b =3 ) and a !=3 and a in
+      (1,"2",'3') and c not in (1) and d like "ces%_" | GENTIMES startTime start="2020-10-12T10:32:58+0800" end="2020-10-12T10:32:59+0800"
+
+source=table search a=1 and ( b= 2 or b =3 ) and a !=3 and a in
+      (1,"2",'3') and c not in (1) and d like "ces%_" | GENTIMES startTime start="1602470559000" end="1602297759000"
+
+
+湘
+<!-- https://www.ip138.com/utf8/ -->
+utf-8: &#x6E58;
+<!-- http://www.mytju.com/classcode/tools/encode_gb2312.asp -->
+GBK: CFE6
--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,39 @@ const { src, dest } = require("gulp");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
+const header = require("gulp-header");
+var pkg = require("./package.json");
+var banner = [
+  "/**",
+  " * <%= pkg.name %> - <%= pkg.description %>",
+  " * @version v<%= pkg.version %>",
+  " * @author <%= pkg.author %>",
+  " * @buildAt <%= buildTime %>",
+  " */",
+  ""
+].join("\n");
 
 exports.default = function() {
   return (
-    src("lib/splunk-spl-to-sql-converter.js")
+    src("lib/converter.js")
       // es6 => es5
       .pipe(babel())
       // 压缩
-      .pipe(uglify())
+      .pipe(
+        uglify({
+          compress: {
+            negate_iife: false,
+            drop_console: true
+          }
+        })
+      )
+      // 增加生成时间
+      .pipe(
+        header(banner, {
+          pkg,
+          buildTime: new Date().toISOString()
+        })
+      )
       // 重命名
       .pipe(rename({ extname: ".min.js" }))
       .pipe(dest("lib/"))
