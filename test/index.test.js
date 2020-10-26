98a0b3fe2963dfc9dac421d622443da1be623bd6
wangfucheng <wangfucheng@machloop.com>
2020-10-26 14:23:49 +0800

    feat: 更新版本 - 移除 source 字段 - 字段的值移除特殊字符范围限制

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 const converter = require("../index");
 describe("Splunk SPL to SQL test", () => {
-  const spl1 = "source=table | search age=30";
+  const spl1 = "| search age=30";
   test(spl1, () => {
     const dsl1 = converter.parse(spl1, { json: true });
     expect(dsl1).toStrictEqual({
       result: {
-        source: "source=table | search age=30",
-        target: "`age`=:param_1_22_1_28",
-        params: { param_1_22_1_28: "30" },
+        source: "| search age=30",
+        target: "`age`=:param_1_9_1_15",
+        params: { param_1_9_1_15: "30" },
         dev: {
-          expression: { WHERE: "`age`=:param_1_22_1_28" },
+          expression: { WHERE: "`age`=:param_1_9_1_15" },
           fields: ["age"]
         }
       }
